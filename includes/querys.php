ca3516abae2b67e43977c1a5ae7599b128a27b86
Vitor-Oliveira-Villela <vitorvillelath@gmail.com>
2025-09-17 16:09:28 -0300

    Ajustes na query de validação de login

--- includes/querys.php
+++ includes/querys.php
@@ -9,6 +9,27 @@ class Querys
         $conn = new ConnectionDB();
         $pdo = $conn->connect();
 
+        $qry = $pdo->prepare("SELECT * FROM users WHERE username = :username and password = :password");
+        $qry->execute(["username" => $username, "password" => $password] );
+        $result = $qry->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) 
+        {
+            $lRet = true;
+        } 
+        else 
+        {
+            $lRet = false;
+        }
+
+        return $lRet;
+    } 
+    /*
+    public function validaLogin($username, $password)
+    {
+        $conn = new ConnectionDB();
+        $pdo = $conn->connect();
+
         $qry = $pdo->prepare("SELECT * FROM users WHERE username = :username");
         $qry->execute(["username" => $username]);
         $result = $qry->fetch(PDO::FETCH_ASSOC);
@@ -24,4 +45,5 @@ class Querys
 
         return $lRet;
     } 
+    */
 }
